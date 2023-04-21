904ece142df2fc8b66e1ea7b4efa22d594a7868f
Lokesh <lokeshsingh7695@gmail.com>
2023-04-21 06:25:06 +0530

    Day 1

---
+++ Syntax.c
@@ -0,0 +1,20 @@
+#include<stdio.h> // Include Header File 
+int main()
+{
+printf("Hello world!\n");
+return 0;
+}
+
+/*
+The basic syntax of the C program consists of header, main() function, variable declaration, body, and return type of the program.
+
+The header is the first line in the C program with extension .h which contains macro definitions and C functions. 
+
+Programs must contain main() function, because execution in C programming starts from main().
+
+Variable declaration in C is done inside the main function and can be used in the body anywhere but before the main, we can also declare variables which are known as Global variables. 
+
+In the body of the function, we perform operations required inside the function like printing, sum, average, sorting, searching, etc.
+
+The last part of the C program is the return statement which refers to returning values of the program. If the return type is void then there will be no return statement.
+*/
