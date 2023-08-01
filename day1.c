e7c6111b34a13ac5b43328014006dea7a769cf66
Lokesh <lokeshsingh7695@gmail.com>
2023-08-01 10:57:40 +0530

    first push

---
+++ day1.c
@@ -0,0 +1,17 @@
+// This is SIngle Line Comment
+
+/*
+    This is Multi Line Comment
+*/
+
+#include <stdio.h> // Header File Which Have Already Some Code for executing basic Function like printf and scanf
+
+int main()
+{ // main function of the code where the program execution begins(also called entry point of the program) ans "{" shows the entry point of the function
+
+    printf("Hello World\n"); // Printf is is standard funtion which is used to print something on the standard console(monitor)
+                             // ans "\n" is a escape sequence character used to print new line
+
+    return 0; // return 0 means program executed successfully
+
+} // This is end point of the function
