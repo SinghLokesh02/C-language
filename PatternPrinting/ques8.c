8ee6662b145ba6541f7abc78a042fd767c6c4d5a
Lokesh <lokeshsingh7695@gmail.com>
2024-04-12 06:55:07 +0530

    Question 8 of C

---
+++ PatternPrinting/ques8.c
@@ -0,0 +1,34 @@
+/*
+8) Write a Program to Print the Following Pattern.
+
+For n = 5 (Take n as input from user)
+
+1 2 3 4 5
+ 2 3 4 5
+  3 4 5
+   4 5
+    5
+*/
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number of Row You want to Print : ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {   
+        for(int space = 1;space<=i-1;space++){
+            printf(" ");
+        }
+
+        for (int j = i; j <= n; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
